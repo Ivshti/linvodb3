a208cb86f93e5af108bc7d2df122978168473342
Ivshti <ivo@strem.io>
2016-02-19 23:20:04 +0200

    sample: basic displaying of results

--- sample/sample.js
+++ sample/sample.js
@@ -1,2 +1,25 @@
 var linvodb = require('linvodb')
-var leveljs = require('level-js')
+
+// Use leveljs (IndexedDB wrapper) for storage
+linvodb.defaults.store = { db: require('level-js') };
+
+var Task = new linvodb('task', {
+	name: String,
+	description: String,
+	created: Date,
+	due: Date,
+	completed: Boolean
+});
+
+Task.on('construct', function(task) {
+	task.due = new Date(Date.now() + 24*60*60*1000)
+})
+
+var app = angular.module('todo', [])
+
+app.controller('todoList', ['$scope', function($scope) {
+	$scope.tasks = Task.find({ }).live()
+	$scope.incomplete = Task.find({ completed: false }).count().live()
+
+	Task.on('liveQueryUpdate', function() { $scope.$digest() })
+}])
